691efb416e1736cfdac9677843da6f7d0c0c91c1
R.Fuentes <R.Fuentes@RFuentes-PC>
2018-05-02 13:24:11 -0600

    Se agregaron las clases paquete basico,reservacion y admin hotel 2

--- src/primerparcialpoo/AdminHotel.java
+++ src/primerparcialpoo/AdminHotel.java
@@ -10,5 +10,27 @@ package primerparcialpoo;
  * @author R.Fuentes
  */
 public class AdminHotel {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public AdminHotel(){}
+    
+    
+    public void modificarPrecio(){
+}
+    public void modificarPaquete(){
+}
+    public void modificarHabitacion(){
+}
+    public void modificarPiso(){
+}
+    
     
 }
--- src/primerparcialpoo/PaqueteBasico.java
+++ src/primerparcialpoo/PaqueteBasico.java
@@ -10,5 +10,48 @@ package primerparcialpoo;
  * @author R.Fuentes
  */
 public class PaqueteBasico {
+    public String idPaquete; 
+    public boolean piscina,internet; 
+    public Precio costoExtra;
     
+    public PaqueteBasico(){}
+    
+    public PaqueteBasico(String idPaquete, boolean piscina, boolean internet, Precio costoExtra){
+        this.idPaquete=idPaquete;
+        this.piscina=piscina;
+         
+   }
+
+    public String getIdPaquete() {
+        return idPaquete;
+    }
+
+    public void setIdPaquete(String idPaquete) {
+        this.idPaquete = idPaquete;
+    }
+
+    public boolean isPiscina() {
+        return piscina;
+    }
+
+    public void setPiscina(boolean piscina) {
+        this.piscina = piscina;
+    }
+
+    public boolean isInternet() {
+        return internet;
+    }
+
+    public void setInternet(boolean internet) {
+        this.internet = internet;
+    }
+
+    public Precio getCostoExtra() {
+        return costoExtra;
+    }
+
+    public void setCostoExtra(Precio costoExtra) {
+        this.costoExtra = costoExtra;
+    }
+   
 }
