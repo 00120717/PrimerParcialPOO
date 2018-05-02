62ee20c8cddaa75147982929387b27cc35220d91
MMont <MMont@LAPTOP-KCJJ3911>
2018-05-02 11:44:22 -0600

    Se Agrego clase admReservacion

---
+++ src/primerparcialpoo/AdmReservacion.java
@@ -0,0 +1,26 @@
+package primerparcialpoo;
+
+/**
+ *
+ * @author 00161617
+ */
+public class AdmReservacion {
+    
+    public AdmReservacion(){
+    }
+    
+    public void addReservacion(){
+        
+    }
+    public void verReservacion(int id){
+    }
+   
+    public void modificarReservacion(){
+        
+    }
+    public void calcelarReservacion(){
+        
+    }
+    
+    
+}
